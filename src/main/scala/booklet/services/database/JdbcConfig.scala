46f5b610edb45148318509ac3c56ef733282fdbe
kchapl <kchapl@users.noreply.github.com>
2021-07-15 21:29:03 +0100

    Build JDBC URL

---
+++ src/main/scala/booklet/services/database/JdbcConfig.scala
@@ -0,0 +1,20 @@
+package booklet.services.database
+
+import java.net.URI
+
+case class JdbcConfig(url: String, userName: Option[String], password: Option[String])
+
+object JdbcConfig {
+
+  def fromDbUrl(dbUrl: String) = {
+    val uri = new URI(dbUrl)
+    val userInfo = Option(uri.getUserInfo).map(_.split(":"))
+    val port = if (uri.getPort == -1) "" else s":${uri.getPort}"
+    val sslMode = if (uri.getHost == "localhost") "" else "?sslmode=require"
+    JdbcConfig(
+      url = s"jdbc:postgresql://${uri.getHost}$port${uri.getPath}$sslMode",
+      userName = userInfo.flatMap(_.headOption),
+      password = userInfo.flatMap(_.lift(1))
+    )
+  }
+}
