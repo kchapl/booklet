1ecfebd24c9998ea022443644a30c9c56f743762
kchapl <kchapl@users.noreply.github.com>
2021-10-27 21:18:57 +0100

    Add book finder implementation

--- src/main/scala/booklet/services/book_finder/BookFinder.scala
+++ src/main/scala/booklet/services/book_finder/BookFinder.scala
@@ -2,19 +2,40 @@ package booklet.services.book_finder
 
 import booklet.Failure
 import booklet.model.BookData
-import zio.{Has, ZIO, ZLayer}
+import booklet.services.book_finder.Model.GoogleBookResult
+import okhttp3.{OkHttpClient, Request}
+import upickle.legacy.read
+import zio.{Has, UIO, ULayer, ZIO}
 
 trait BookFinder {
   def findByIsbn(isbn: String): ZIO[Any, Failure, Option[BookData]]
 }
 
 object BookFinder {
-
   def findByIsbn(isbn: String): ZIO[Has[BookFinder], Failure, Option[BookData]] =
     ZIO.serviceWith(_.findByIsbn(isbn))
+}
 
-  val live: ZLayer[Any, Nothing, Has[BookFinder]] =
-    ZLayer.succeed { _ =>
-      ZIO.some(BookData(None, None, None, None, None, None))
-    }
+object BookFinderLive {
+
+  private val client = new OkHttpClient()
+
+  private def request(isbn: String) = new Request.Builder()
+    .url(s"https://www.googleapis.com/books/v1/volumes?q=isbn:$isbn")
+    .build()
+
+  private val effect: UIO[BookFinder] = UIO.succeed(isbn =>
+    for {
+      // TODO close response
+      response <- ZIO
+        .effect(client.newCall(request(isbn)).execute())
+        .mapError(Failure.fromThrowable)
+      responseBody = response.body.string
+      result <-
+        ZIO.effect(read[GoogleBookResult](responseBody)).mapError(Failure.fromThrowable)
+      book = GoogleBookResult.toBook(result)
+    } yield book
+  )
+
+  val layer: ULayer[Has[BookFinder]] = effect.toLayer
 }
