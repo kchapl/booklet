cc751380bf315db927ef46b064f4e245adbe8939
kchapl <kchapl@users.noreply.github.com>
2021-10-27 20:04:01 +0100

    Refactor

--- src/main/scala/booklet/services/database/Database.scala
+++ src/main/scala/booklet/services/database/Database.scala
@@ -5,18 +5,22 @@ import booklet.model._
 import zio._
 
 trait Database {
-  val fetchAllBooks: ZIO[Any, Failure, List[Book]]
-  val fetchAllReadings: ZIO[Any, Failure, List[Reading]]
+  val fetchAllBooks: IO[Failure, List[Book]]
+  val fetchAllReadings: IO[Failure, List[Reading]]
 
-  def fetchBook(id: BookId): ZIO[Any, Failure, Option[Book]]
-  def fetchReading(id: ReadingId): ZIO[Any, Failure, Option[Reading]]
+  def fetchBook(id: BookId): IO[Failure, Option[Book]]
 
-  def insertBook(data: BookData): ZIO[Any, Failure, Unit]
-  def insertReading(data: ReadingData): ZIO[Any, Failure, Unit]
+  def fetchReading(id: ReadingId): IO[Failure, Option[Reading]]
 
-  def updateBook(id: BookId, data: BookData): ZIO[Any, Failure, Unit]
-  def updateReading(id: ReadingId, data: ReadingData): ZIO[Any, Failure, Unit]
+  def insertBook(data: BookData): IO[Failure, Unit]
 
-  def deleteBook(id: BookId): ZIO[Any, Failure, Unit]
-  def deleteReading(id: ReadingId): ZIO[Any, Failure, Unit]
+  def insertReading(data: ReadingData): IO[Failure, Unit]
+
+  def updateBook(id: BookId, data: BookData): IO[Failure, Unit]
+
+  def updateReading(id: ReadingId, data: ReadingData): IO[Failure, Unit]
+
+  def deleteBook(id: BookId): IO[Failure, Unit]
+
+  def deleteReading(id: ReadingId): IO[Failure, Unit]
 }
