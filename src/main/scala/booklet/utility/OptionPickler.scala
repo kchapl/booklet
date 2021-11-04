50bb0b862819695675c259f0b348b0bf1995af0d
kchapl <kchapl@users.noreply.github.com>
2021-11-04 21:09:51 +0000

    Add option pickler

---
+++ src/main/scala/booklet/utility/OptionPickler.scala
@@ -0,0 +1,9 @@
+package booklet.utility
+
+// See https://com-lihaoyi.github.io/upickle/#CustomConfiguration
+object OptionPickler extends upickle.AttributeTagged {
+  implicit override def OptionReader[T: Reader]: Reader[Option[T]] =
+    new Reader.Delegate[Any, Option[T]](implicitly[Reader[T]].map(Some(_))) {
+      override def visitNull(index: Int): Option[T] = None
+    }
+}
