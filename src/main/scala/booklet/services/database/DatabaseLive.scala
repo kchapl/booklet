b022f69a633b4e1464c3b31583cd76a742e20e5f
kchapl <kchapl@users.noreply.github.com>
2021-11-11 19:32:48 +0000

    Refactor

--- src/main/scala/booklet/services/database/DatabaseLive.scala
+++ src/main/scala/booklet/services/database/DatabaseLive.scala
@@ -5,6 +5,7 @@ import booklet.{Config, Failure}
 import cats.effect.{ContextShift, IO}
 import doobie.implicits._
 import doobie.util.Get
+import doobie.util.fragment.Fragment
 import doobie.util.fragments.setOpt
 import doobie.{Put, Read, Transactor}
 import zio._
@@ -16,6 +17,58 @@ import java.util.Date
 import scala.concurrent.ExecutionContext
 
 object DatabaseLive {
+
+  private object Query {
+
+    val fetchAllBooks: Fragment =
+      fr"SELECT id, isbn, author, title FROM books"
+
+    val fetchAllReadings: Fragment =
+      fr"SELECT r.id, b.id, b.isbn, b.author, b.title, r.completed, r.rating" ++
+        fr"FROM books b JOIN readings r" ++
+        fr"ON r.book_id = b.id"
+
+    def fetchBook(id: BookId): Fragment =
+      fr"SELECT id, isbn, author, title FROM books WHERE id = ${id.value}"
+
+    def fetchReading(id: ReadingId): Fragment =
+      fr"SELECT r.id, b.id, b.isbn, b.author, b.title, r.completed, r.rating" ++
+        fr"FROM books" ++
+        fr"JOIN readings r" ++
+        fr"ON r.book_id = b.id" ++
+        fr"WHERE r.id = $id"
+
+    def insertBook(data: BookData): Fragment =
+      fr"INSERT INTO books(isbn, author, title)" ++
+        fr"VALUES(${data.isbn}, ${data.author}, ${data.title})"
+
+    def insertReading(data: ReadingData): Fragment =
+      fr"INSERT INTO readings(book_id, completed, rating)" ++
+        fr"VALUES(${data.bookId}, ${data.completed}, ${data.rating})"
+
+    def updateBook(id: BookId, data: BookData): Fragment =
+      fr"UPDATE books" ++ setOpt(
+        data.isbn.map(isbn => fr"isbn = ${isbn.value}"),
+        data.author.map(author => fr"author = ${author.value}"),
+        data.title.map(title => fr"title = ${title.value}")
+      ) ++ fr"WHERE id=$id"
+
+    def updateReading(id: ReadingId, data: ReadingData): Fragment =
+      fr"UPDATE readings" ++ setOpt(
+        data.completed.map(completed => fr"completed = ${completed.toString}"),
+        data.rating.map(rating => fr"rating = ${rating.value}")
+      ) ++ fr"WHERE id=$id"
+
+    def deleteBook(id: BookId): Fragment =
+      fr"DELETE" ++
+        fr"FROM books" ++
+        fr"WHERE id = $id"
+
+    def deleteReading(id: ReadingId): Fragment =
+      fr"DELETE" ++
+        fr"FROM readings" ++
+        fr"WHERE id = $id"
+  }
 
   implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
 
@@ -35,148 +88,141 @@ object DatabaseLive {
       Reading(ReadingId(id), book, completed, Rating(rating))
     }
 
-  val layer: ZLayer[Has[Config], Failure, Has[Database]] = {
-    val effect = for {
-      config <- ZIO.service[Config]
-    } yield {
-      val xa = {
-        val jdbc = JdbcConfig.fromDbUrl(config.db.url)
-        Transactor
-          .fromDriverManager[Task](
-            driver = config.db.driver,
-            url = jdbc.url,
-            user = jdbc.userName.getOrElse(""),
-            pass = jdbc.password.getOrElse("")
-          )
-      }
-      toDatabase(xa)
-    }
-    effect.toLayer
-  }
+  private def fetchAllBooksImpl(xa: Transactor[Task]) =
+    Query.fetchAllBooks
+      .query[Book]
+      .to[List]
+      .transact(xa)
+      .mapError(Failure.fromThrowable)
+
+  private def fetchAllReadingsImpl(xa: Transactor[Task]) =
+    Query.fetchAllReadings
+      .query[Reading]
+      .to[List]
+      .transact(xa)
+      .mapError(Failure.fromThrowable)
+
+  private def fetchBookImpl(xa: Transactor[Task], id: BookId) =
+    Query
+      .fetchBook(id)
+      .query[Book]
+      .option
+      .transact(xa)
+      .mapError(Failure.fromThrowable)
+
+  private def fetchReadingImpl(xa: Transactor[Task], id: ReadingId) =
+    Query
+      .fetchReading(id)
+      .query[Reading]
+      .option
+      .transact(xa)
+      .mapError(Failure.fromThrowable)
+
+  private def InsertBookImpl(xa: Transactor[Task], data: BookData) =
+    Query
+      .insertBook(data)
+      .update
+      .withUniqueGeneratedKeys[Long]("id")
+      .transact(xa)
+      .mapBoth(
+        Failure.fromThrowable,
+        _ => ()
+      )
+
+  private def insertReadingImpl(xa: Transactor[Task], data: ReadingData) =
+    Query
+      .insertReading(data)
+      .update
+      .withUniqueGeneratedKeys[Long]("id")
+      .transact(xa)
+      .mapBoth(
+        Failure.fromThrowable,
+        _ => ()
+      )
+
+  private def updateBookImpl(xa: Transactor[Task], id: BookId, data: BookData) =
+    Query
+      .updateBook(id, data)
+      .update
+      .run
+      .transact(xa)
+      .mapBoth(
+        Failure.fromThrowable,
+        _ => ()
+      )
+
+  private def updateReadingImpl(xa: Transactor[Task], id: ReadingId, data: ReadingData) =
+    Query
+      .updateReading(id, data)
+      .update
+      .run
+      .transact(xa)
+      .mapBoth(
+        Failure.fromThrowable,
+        _ => ()
+      )
+
+  private def deleteBookImpl(xa: Transactor[Task], id: BookId) =
+    Query
+      .deleteBook(id)
+      .update
+      .run
+      .transact(xa)
+      .mapBoth(
+        Failure.fromThrowable,
+        _ => ()
+      )
+
+  private def deleteReadingImpl(xa: Transactor[Task], id: ReadingId) =
+    Query
+      .deleteReading(id)
+      .update
+      .run
+      .transact(xa)
+      .mapBoth(
+        Failure.fromThrowable,
+        _ => ()
+      )
 
   private def toDatabase(xa: Transactor[Task]): Database =
     new Database {
+      val fetchAllBooks: ZIO[Any, Failure, List[Book]] = fetchAllBooksImpl(xa)
+      val fetchAllReadings: ZIO[Any, Failure, List[Reading]] = fetchAllReadingsImpl(xa)
 
-      val fetchAllBooks: ZIO[Any, Failure, List[Book]] =
-        sql"""
-             |SELECT id, isbn, author, title
-             |FROM books
-             |""".stripMargin
-          .query[Book]
-          .to[List]
-          .transact(xa)
-          .mapError(Failure.fromThrowable)
+      def fetchBook(id: BookId): ZIO[Any, Failure, Option[Book]] = fetchBookImpl(xa, id)
 
-      val fetchAllReadings: ZIO[Any, Failure, List[Reading]] =
-        sql"""
-             |SELECT r.id, b.id, b.isbn, b.author, b.title, r.completed, r.rating
-             |FROM books b
-             |JOIN readings r 
-             |ON r.book_id = b.id
-             |""".stripMargin
-          .query[Reading]
-          .to[List]
-          .transact(xa)
-          .mapError(Failure.fromThrowable)
+      def fetchReading(id: ReadingId): ZIO[Any, Failure, Option[Reading]] = fetchReadingImpl(xa, id)
 
-      def fetchBook(id: BookId): ZIO[Any, Failure, Option[Book]] =
-        sql"""
-             |SELECT id, isbn, author, title
-             |FROM books
-             |WHERE id = $id
+      def insertBook(data: BookData): ZIO[Any, Failure, Unit] = InsertBookImpl(xa, data)
 
-             |""".stripMargin
-          .query[Book]
-          .option
-          .transact(xa)
-          .mapError(Failure.fromThrowable)
+      def insertReading(data: ReadingData): ZIO[Any, Failure, Unit] = insertReadingImpl(xa, data)
 
-      def fetchReading(id: ReadingId): ZIO[Any, Failure, Option[Reading]] =
-        sql"""
-             |SELECT r.id, b.id, b.isbn, b.author, b.title, r.completed, r.rating
-             |FROM books b
-             |JOIN readings r 
-             |ON r.book_id = b.id
-             |WHERE r.id = $id
-             |""".stripMargin
-          .query[Reading]
-          .option
-          .transact(xa)
-          .mapError(Failure.fromThrowable)
+      def updateBook(id: BookId, data: BookData): ZIO[Any, Failure, Unit] =
+        updateBookImpl(xa, id, data)
 
-      def insertBook(data: BookData): ZIO[Any, Failure, Unit] =
-        sql"""
-             |INSERT INTO books(isbn, author, title)
-             |VALUES (${data.isbn}, ${data.author}, ${data.title})
-             |""".stripMargin.update
-          .withUniqueGeneratedKeys[Long]("id")
-          .transact(xa)
-          .mapBoth(
-            Failure.fromThrowable,
-            _ => ()
-          )
+      def updateReading(id: ReadingId, data: ReadingData): ZIO[Any, Failure, Unit] =
+        updateReadingImpl(xa, id, data)
 
-      def insertReading(data: ReadingData): ZIO[Any, Failure, Unit] =
-        sql"""
-             |INSERT INTO readings(book_id, completed, rating)
-             |VALUES (${data.bookId}, ${data.completed}, ${data.rating})
-             |""".stripMargin.update
-          .withUniqueGeneratedKeys[Long]("id")
-          .transact(xa)
-          .mapBoth(
-            Failure.fromThrowable,
-            _ => ()
-          )
+      def deleteBook(id: BookId): ZIO[Any, Failure, Unit] = deleteBookImpl(xa, id)
 
-      def updateBook(id: BookId, data: BookData): ZIO[Any, Failure, Unit] = {
-        val q = fr"UPDATE books" ++ setOpt(
-          data.isbn.map(isbn => fr"isbn = ${isbn.value}"),
-          data.author.map(author => fr"author = ${author.value}"),
-          data.title.map(title => fr"title = ${title.value}")
-        ) ++ fr"WHERE id=$id"
-
-        q.update.run
-          .transact(xa)
-          .mapBoth(
-            Failure.fromThrowable,
-            _ => ()
-          )
-      }
-
-      def updateReading(id: ReadingId, data: ReadingData): ZIO[Any, Failure, Unit] = {
-        val q = fr"UPDATE readings" ++ setOpt(
-          data.completed.map(completed => fr"completed = ${completed.toString}"),
-          data.rating.map(rating => fr"rating = ${rating.value}")
-        ) ++ fr"WHERE id=$id"
-
-        q.update.run
-          .transact(xa)
-          .mapBoth(
-            Failure.fromThrowable,
-            _ => ()
-          )
-      }
-
-      def deleteBook(id: BookId): ZIO[Any, Failure, Unit] =
-        sql"""
-             |DELETE FROM books
-             |WHERE id = $id
-             |""".stripMargin.update.run
-          .transact(xa)
-          .mapBoth(
-            Failure.fromThrowable,
-            _ => ()
-          )
-
-      def deleteReading(id: ReadingId): ZIO[Any, Failure, Unit] =
-        sql"""
-           |DELETE FROM readings
-           |WHERE id = $id
-           |""".stripMargin.update.run
-          .transact(xa)
-          .mapBoth(
-            Failure.fromThrowable,
-            _ => ()
-          )
+      def deleteReading(id: ReadingId): ZIO[Any, Failure, Unit] = deleteReadingImpl(xa, id)
     }
+
+  private val effect = for {
+    config <- ZIO.service[Config]
+  } yield {
+    val xa = {
+      val jdbc = JdbcConfig.fromDbUrl(config.db.url)
+      Transactor
+        .fromDriverManager[Task](
+          driver = config.db.driver,
+          url = jdbc.url,
+          user = jdbc.userName.getOrElse(""),
+          pass = jdbc.password.getOrElse("")
+        )
+    }
+    toDatabase(xa)
+  }
+
+  val layer: ZLayer[Has[Config], Failure, Has[Database]] = effect.toLayer
 }
