b44c59c4212ec9c26ae2a15f705381afb09f0825
kchapl <kchapl@users.noreply.github.com>
2022-03-13 19:52:54 +0000

    Refactor

--- src/main/scala/booklet/services/book_handler/BookHandlerLive.scala
+++ src/main/scala/booklet/services/book_handler/BookHandlerLive.scala
@@ -16,79 +16,89 @@ object BookHandlerLive {
       .fromOption(bookId.toLongOption)
       .map(BookId(_))
 
-  private def fromDatabase(db: Database) =
-    new BookHandler {
+  private def fetchAllImpl(db: Database): UIO[Response] =
+    db.fetchAllBooks
+      .fold(
+        serverFailure,
+        books => ok(BookView.list(books).toString)
+      )
 
-      def fetchAll: UIO[Response] =
-        db.fetchAllBooks
-          .fold(
-            serverFailure,
-            books => ok(BookView.list(books).toString)
-          )
+  private def fetchImpl(db: Database, bookId: String): UIO[Response] =
+    toBookId(bookId)
+      .foldM(
+        _ => ZIO.succeed(badRequest(s"Cannot parse ID $bookId")),
+        id =>
+          db
+            .fetchBook(id)
+            .fold(
+              serverFailure,
+              {
+                case None       => notFound(Path(bookId))
+                case Some(book) => ok(BookView.list(Seq(book)).toString)
+              }
+            )
+      )
 
-      def fetch(bookId: String): UIO[Response] =
-        toBookId(bookId)
+  private def createImpl(db: Database, request: Request): IO[Failure, Response] =
+    Query
+      .fromRequest(request)
+      .flatMap(requestQry =>
+        ZIO
+          .fromOption(BookData.completeFromHttpQuery(requestQry))
           .foldM(
-            _ => ZIO.succeed(badRequest(s"Cannot parse ID $bookId")),
-            id =>
+            _ => ZIO.succeed(badRequest(requestQry.toString)),
+            bookData =>
               db
-                .fetchBook(id)
+                .insertBook(bookData)
                 .fold(
                   serverFailure,
-                  {
-                    case None       => notFound(Path(bookId))
-                    case Some(book) => ok(BookView.list(Seq(book)).toString)
-                  }
+                  _ => seeOther(path = "/books")
                 )
           )
+      )
 
-      def create(request: Request): IO[Failure, Response] =
-        Query
-          .fromRequest(request)
-          .flatMap(requestQry =>
-            ZIO
-              .fromOption(BookData.completeFromHttpQuery(requestQry))
-              .foldM(
-                _ => ZIO.succeed(badRequest(requestQry.toString)),
-                bookData =>
-                  db
-                    .insertBook(bookData)
-                    .fold(
-                      serverFailure,
-                      _ => seeOther(path = "/books")
-                    )
-              )
-          )
-
-      def update(bookId: String)(request: Request): IO[Failure, Response] =
-        Query
-          .fromRequest(request)
-          .flatMap(requestQry =>
-            toBookId(bookId)
-              .foldM(
-                _ => ZIO.succeed(badRequest(requestQry.toString)),
-                id =>
-                  db
-                    .updateBook(id, BookData.partialFromHttpQuery(requestQry))
-                    .fold(
-                      serverFailure,
-                      _ => seeOther("/books")
-                    )
-              )
-          )
-
-      def delete(bookId: String): UIO[Response] =
+  private def updateImpl(db: Database, bookId: String)(request: Request): IO[Failure, Response] =
+    Query
+      .fromRequest(request)
+      .flatMap(requestQry =>
         toBookId(bookId)
           .foldM(
-            _ => ZIO.succeed(badRequest(s"Cannot parse ID $bookId")),
+            _ => ZIO.succeed(badRequest(requestQry.toString)),
             id =>
               db
-                .deleteBook(id)
+                .updateBook(id, BookData.partialFromHttpQuery(requestQry))
                 .fold(
                   serverFailure,
                   _ => seeOther("/books")
                 )
           )
+      )
+
+  private def deleteImpl(db: Database, bookId: String): UIO[Response] =
+    toBookId(bookId)
+      .foldM(
+        _ => ZIO.succeed(badRequest(s"Cannot parse ID $bookId")),
+        id =>
+          db
+            .deleteBook(id)
+            .fold(
+              serverFailure,
+              _ => seeOther("/books")
+            )
+      )
+
+  private def fromDatabase(db: Database) =
+    new BookHandler {
+      override val fetchAll: UIO[Response] = fetchAllImpl(db)
+
+      override def fetch(bookId: String): UIO[Response] = fetchImpl(db, bookId)
+
+      override def create(request: Request): IO[Failure, Response] = createImpl(db, request)
+
+      override def update(bookId: String)(request: Request): IO[Failure, Response] =
+        updateImpl(db, bookId)(request)
+
+      override def delete(bookId: String): UIO[Response] = deleteImpl(db, bookId)
     }
 
   private val effect: URIO[Has[Database], BookHandler] =
